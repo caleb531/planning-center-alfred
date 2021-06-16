3119aa3845d2e227b35beeeb3521fb517fc8f7b0
Caleb Evans <caleb@calebevans.me>
2021-06-15 19:26:19 -0700

    Add subtitle to results for clarity

--- pca/filter_people.py
+++ pca/filter_people.py
@@ -14,6 +14,7 @@ def get_result_from_person(person):
 
     return {
         'title': person['attributes']['name'],
+        'subtitle': 'View in Planning Center',
         'arg': '{base_url}/AC{person_id}'.format(
             base_url='https://people.planningcenteronline.com/people',
             person_id=person['id'])
