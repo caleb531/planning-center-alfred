c7c2fb22507754343ed970bb6573196f3a4cad9e
Caleb Evans <caleb@calebevans.me>
2021-06-18 10:37:34 -0700

    Add module for copying email address of selected person

---
+++ pca/copy_email.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from __future__ import print_function, unicode_literals
+
+import json
+import os
+
+import pca.core as core
+
+
+# Return the textual email address for the person with the given ID
+def get_person_email_addr(person_id):
+
+    person_emails = core.fetch_data('/people/v2/people/{id}/emails'.format(
+        id=person_id))
+
+    for email in person_emails:
+        if email['attributes']['primary']:
+            return email['attributes']['address']
+
+    # If there is no primary email, but there is at least one email address on
+    # file, then fall back to the first one
+    if person_emails:
+        return person_emails[0]['attributes']['address']
+
+    # Otherwise, indicate that there's no email number on file for this person
+    return 'N/A'
+
+
+def main(variables):
+
+    person_email_addr = get_person_email_addr(variables['person_id'])
+    print(json.dumps({
+        'alfredworkflow': {
+            'arg': variables['person_id'],
+            'variables': {
+                'person_email_addr': person_email_addr
+            }
+        }
+    }))
+
+
+if __name__ == '__main__':
+    main({
+        'person_id': int(os.environ['person_id'])
+    })
